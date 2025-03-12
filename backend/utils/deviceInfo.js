cd9e2f5f680f3c0ef1d13f64a65d8890139fa978
mfortea <mforteadugo@gmail.com>
2025-03-12 20:33:06 +0100

    Fallo en producción. Tipo de dispositivo

--- backend/utils/deviceInfo.js
+++ backend/utils/deviceInfo.js
@@ -1,14 +1,19 @@
+// utils/deviceInfo.js
+const UAParser = require('ua-parser-js');
+
 const getDeviceInfo = (userAgent) => {
-    const parser = new UAParser();
-    const result = parser.setUA(userAgent).getResult();
-  
-    const os = result.os.name || "Desconocido";
-    const browser = result.browser.name || "Desconocido";
-  
-    let deviceType = result.device.type || "equipo";
-    if (deviceType === "mobile") {
-      deviceType = "smartphone"; 
-    }
-  
-    return { os, browser, deviceType };
-  };
+  const parser = new UAParser();
+  const result = parser.setUA(userAgent).getResult();
+
+  const os = result.os.name || "Desconocido";
+  const browser = result.browser.name || "Desconocido";
+
+  let deviceType = result.device.type || "equipo";
+  if (deviceType === "mobile") {
+    deviceType = "smartphone"; 
+  }
+
+  return { os, browser, deviceType };
+};
+
+module.exports = getDeviceInfo; 
