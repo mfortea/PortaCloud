86b5711ab438b80dec1438e1fa9dcf4c7bf26ea4
mfortea <mforteadugo@gmail.com>
2025-03-12 20:27:14 +0100

    Error de producción arreglado al conectarse un smartphone

--- backend/utils/deviceInfo.js
+++ backend/utils/deviceInfo.js
@@ -1,15 +1,14 @@
-// utils/deviceInfo.js
-const UAParser = require('ua-parser-js');
-
 const getDeviceInfo = (userAgent) => {
-  const parser = new UAParser();
-  const result = parser.setUA(userAgent).getResult();
-
-  const os = result.os.name || "Desconocido";
-  const browser = result.browser.name || "Desconocido";
-  const deviceType = result.device.type || "equipo";
-
-  return { os, browser, deviceType };
-};
-
-module.exports = getDeviceInfo; 
+    const parser = new UAParser();
+    const result = parser.setUA(userAgent).getResult();
+  
+    const os = result.os.name || "Desconocido";
+    const browser = result.browser.name || "Desconocido";
+  
+    let deviceType = result.device.type || "equipo";
+    if (deviceType === "mobile") {
+      deviceType = "smartphone"; 
+    }
+  
+    return { os, browser, deviceType };
+  };
