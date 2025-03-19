efd6d6079496b63074e3f9f0efba3e84fa9433bb
mfortea <mforteadugo@gmail.com>
2025-03-19 17:09:29 +0100

    Dependencia cambiada

--- backend/utils/cleaner.js
+++ backend/utils/cleaner.js
@@ -2,6 +2,7 @@
 const ContentRegistry = require("../models/ContentRegistry");
 const fs = require("fs");
 const path = require("path");
+const Device = require("../models/Device");
 
 const cleanOrphanFiles = async () => {
   try {
